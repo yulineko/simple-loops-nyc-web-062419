5770e4cacaf39dc5148a1e69086ea72df7d4efbf
Kellye Greene <kellyecodes@gmail.com>
2019-04-16 14:20:10 -0400

    Removes duplicate lines of code

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,22 +16,11 @@ def loop_iterator(number_of_times)
     counter += 1
     break if counter == number_of_times
   end
-  
-  counter = 0
-  loop do 
-    puts phrase
-    counter += 1
-    break if counter == number_of_times
-  end
-
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do 
-    puts phrase 
-  
   number_of_times.times do 
     puts phrase 
   end
