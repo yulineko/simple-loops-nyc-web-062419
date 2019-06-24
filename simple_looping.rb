7ca2e67d8654e6d894766f61c64e6df4645ab1ca
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-24 21:43:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
   loop do
     puts phrase
   end 
